0c6b5db00f3126d2e0a9692be5da0a5c0fe1c0e3
ozberkgunes <ozberk_07@hotmail.com>
2021-10-10 11:02:37 +0300

    ParitalView kullanımı ve razor web page uygulaması

--- Travel_Web/Controllers/DefaultController.cs
+++ Travel_Web/Controllers/DefaultController.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using travel_web.Models.DAL;
 namespace travel_web.Controllers
 {
     public class DefaultController : Controller
     {
         // GET: Default
+        Contex c = new Contex();
         public ActionResult Index()
         {
-            return View();
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
+        }
+        public PartialViewResult partialIndex()
+        {
+            var bloglar = c.Blogs.Take(3).ToList();
+            return PartialView(bloglar);
         }
         
     }
